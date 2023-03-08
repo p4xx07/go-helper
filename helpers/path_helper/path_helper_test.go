96307cf457534d8235886aedaeb3ce7f252203be
Paxx <daniel.botta@hiway.media>
2023-03-08 16:46:48 +0100

    release: updated the minio client to the v7 amend

--- helpers/path_helper/path_helper_test.go
+++ helpers/path_helper/path_helper_test.go
@@ -16,8 +16,8 @@ func TestPathWithoutExt(t *testing.T) {
 
 func TestBasePathWithoutExt(t *testing.T) {
 	long := "path/without/ext"
-	expected := "ext"
 	ext := ".mp4"
+	expected := "ext"
 
 	value := BasePathWithoutExt(long + ext)
 	if value != expected {
